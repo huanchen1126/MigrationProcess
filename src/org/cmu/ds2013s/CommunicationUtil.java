2537dffbdfb942a1ef6777e7ce10da1dfbea4072
huanchen1126 <huanchen1126@gmail.com>
2013-01-27 15:10:53 -0500

    sendCommand

--- src/org/cmu/ds2013s/CommunicationUtil.java
+++ src/org/cmu/ds2013s/CommunicationUtil.java
@@ -1,6 +1,9 @@
 package org.cmu.ds2013s;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class CommunicationUtil {
@@ -22,5 +25,24 @@ public class CommunicationUtil {
       return null;
     else
       return addr.getHostAddress();
+  }
+  /**
+   * send command
+   * */
+  public static void sendCommand(String ip, int port, byte[] content){
+    Socket socket = null;
+    try {
+      socket = new Socket(ip,port);
+      OutputStream out = socket.getOutputStream(); 
+      out.write(content);
+      out.close();
+      socket.close();
+    } catch (UnknownHostException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
