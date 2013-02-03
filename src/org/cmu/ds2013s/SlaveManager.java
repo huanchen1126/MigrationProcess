d71de19cb99b43b760c8e2c034a059245769fb3a
huanchen1126 <huanchen1126@gmail.com>
2013-02-03 16:52:04 -0500

    commin

--- src/org/cmu/ds2013s/SlaveManager.java
+++ src/org/cmu/ds2013s/SlaveManager.java
@@ -7,26 +7,25 @@ import java.util.Vector;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class SlaveManager implements ManagerContext {
-  //public Vector<Thread> processes;
+  /* processes running on this slave. processes will be removed when terminated */
   HashMap<Thread, MigratableProcess> processes;
-  public Console console;
-
+  
+  /* ip of this slave */
   private String _hostname;
 
+  /* port of this slave */
   private int _port;
 
+  /* ip of master that this slave connect to */
   private String _masterHostname;
 
+  /* port of master that this slave connect to */
   private int _masterPort;
   
   private AtomicInteger currentLoad = new AtomicInteger(0);
-  
-  private AtomicInteger _id = new AtomicInteger(0);
 
   public SlaveManager(int port, String masterHostname, int masterPort) {
-    //processes = new Vector<Thread>();
     processes = new HashMap<Thread, MigratableProcess>();
-    console = System.console();
     this._hostname = CommunicationUtil.getLocalIPAddress();
     this._port = port;
     this._masterHostname = masterHostname;
@@ -39,8 +38,6 @@ public class SlaveManager implements ManagerContext {
     heartBeatSender.start();
     Thread listener = new Thread(new NetworkListener(SlaveMessageHandler.class, _port, this));
     listener.start();
-    //org.cmu.ds2013s.TestMigrateProcess
-    //org.cmu.ds2013s.WebpageCrawler url.txt html2.txt
     try {
       heartBeatSender.join();
       listener.join();
@@ -90,9 +87,4 @@ public class SlaveManager implements ManagerContext {
     this.currentLoad.set(currentLoad);
   }
 
-  public int getId() {
-    return _id.incrementAndGet();
-  }
-
-  
 }
